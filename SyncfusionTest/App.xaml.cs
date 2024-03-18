663c228da9d2b84782ec73dd15e8ea51f6068a34
Marius <marius@fjeld.et>
2024-03-18 12:36:12 +0100

    Add project files.

---
+++ SyncfusionTest/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SyncfusionTest
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ SyncfusionTest/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SyncfusionTest
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
